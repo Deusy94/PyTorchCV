f947980c05d3b5d40db34a0656d0ec8340be4ff9
Donny You <youansheng@gmail.com>
2018-06-13 16:03:59 +0800

    coco det generator.

--- datasets/det/coco/coco_det_generator.py
+++ datasets/det/coco/coco_det_generator.py
@@ -18,11 +18,20 @@ from pycocotools.coco import COCO
 
 JOSN_DIR = 'json'
 IMAGE_DIR = 'image'
+
 CAT_DICT = {
-    'aeroplane': 0, 'bicycle':1,'bird':2,'boat':3,'bottle':4,
-    'bus': 5, 'car': 6, 'cat': 7, 'chair': 8, 'cow': 9, 'diningtable': 10,
-    'dog': 11, 'horse': 12, 'motorbike': 13, 'person': 14, 'pottedplant': 15,
-    'sheep': 16, 'sofa': 17, 'train': 18, 'tvmonitor': 19
+    "person": 1, "bicycle":2, "car": 3, "motorcycle": 4, "airplane": 5, "bus": 6, "train": 7,
+    "truck": 8, "boat": 9, "traffic light": 10, "fire hydrant": 11, "stop sign": 13, "parking meter": 14,
+    "bench": 15, "bird": 16, "cat": 17, "dog": 18, "horse": 19, "sheep": 20, "cow": 21, "elephant": 22,
+    "bear": 23, "zebra": 24, "giraffe": 25, "backpack": 27, "umbrella": 28, "handbag": 31, "tie": 32,
+    "suitcase": 33, "frisbee": 34, "skis": 35, "snowboard": 36, "sports ball": 37, "kite": 38,
+    "baseball bat": 39, "baseball glove": 40, "skateboard": 41, "surfboard": 42, "tennis racket": 43,
+    "bottle": 44, "wine glass": 46, "cup": 47, "fork": 48, "knife": 49, "spoon": 50, "bowl": 51,
+    "banana": 52, "apple": 53, "sandwich": 54, "orange": 55, "broccoli": 56, "carrot": 57, "hot dog": 58,
+    "pizza": 59, "donut": 60, "cake": 61, "chair": 62, "couch": 63, "potted plant": 64, "bed": 65,
+    "dining table": 67, "toilet": 70, "tv": 72, "laptop": 73, "mouse": 74, "remote": 75, "keyboard": 76,
+    "cell phone": 77, "microwave": 78, "oven": 79, "toaster": 80, "sink": 81, "refrigerator": 82,
+    "book": 84, "clock": 85, "vase": 86, "scissors": 87, "teddy bear": 88, "hair drier": 89, "toothbrush": 90
 }
 
 
@@ -45,14 +54,28 @@ class CocoDetGenerator(object):
     def generate_label(self):
         for i, img_id in enumerate(self.img_ids):
             json_dict = dict()
-            ann_ids = self.coco.getAnnIds(imgIds=img_id)
-            img_anns = self.coco.loadAnns(ann_ids)
-            num_persons = len(img_anns)
-            filename = self.coco.imgs[img_id]['file_name']
-            width = self.coco.imgs[img_id]['width']
-            height = self.coco.imgs[img_id]['height']
-            json_dict['height'] = height
-            json_dict['width'] = width
+            file_name = self.coco.imgs[img_id]['file_name']
+            json_dict['width'] = self.coco.imgs[img_id]['width']
+            json_dict['height'] = self.coco.imgs[img_id]['height']
+
+            ann_ids = self.coco.getAnnIds(imgIds=img_id, catIds=self.cat_ids)
+            annos = self.coco.loadAnns(ann_ids)
+            object_list = list()
+            for anno in annos:
+                object_dict = dict()
+                object_dict['label'] = self.cat_ids.index(anno['category_id'])
+                bbox = anno['bbox']
+                object_dict['bbox'] = [float(bbox[0]), float(bbox[1]),
+                                       (float(bbox[2]) + float(bbox[0])), (float(bbox[3]) + float(bbox[1]))]
+
+                object_list.append(object_dict)
+
+            json_dict['objects'] = object_list
+            fw = open(os.path.join(self.json_dir, '{}.json'.format(file_name.split('.')[0])), 'w')
+            fw.write(json.dumps(json_dict))
+            fw.close()
+            shutil.copy(os.path.join(self.args.ori_img_dir, file_name),
+                        os.path.join(self.image_dir, file_name))
 
 
 
